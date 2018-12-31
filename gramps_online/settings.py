c5e62bee38913a5e550080bd1d6e1e1f6548c0e4
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-12-31 17:47:44 +0200

    Use default secret key if env variable not available
    Signed-off-by: Brylie Christopher Oxley <brylie@gnumedia.org>

--- gramps_online/settings.py
+++ gramps_online/settings.py
@@ -19,10 +19,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'this_is_not_secure!'
+DEFAULT_SECRET_KEY = 'this_is_not_secure!'
 
-if os.environ.get('DJANGO_SECRET_KEY'):
-    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', DEFAULT_SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
