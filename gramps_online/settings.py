67cf0730cc6c22669c15a9a8f0ad87e18f3af9fa
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-12-31 22:53:50 +0200

    Add AGPL notice
    Signed-off-by: Brylie Christopher Oxley <brylie@gnumedia.org>

--- gramps_online/settings.py
+++ gramps_online/settings.py
@@ -10,6 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+#  Gramps Online - an online version of the Gramps genealogy program
+#
+#  Copyright (C) 2018-2019 Brylie Christopher Oxley
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
