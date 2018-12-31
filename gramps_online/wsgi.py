f6563feb53eb9cf51d247c589dda4e5edbf71153
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-12-31 23:18:00 +0200

    Ignore invalid-name lint errors
    Signed-off-by: Brylie Christopher Oxley <brylie@gnumedia.org>

--- gramps_online/wsgi.py
+++ gramps_online/wsgi.py
@@ -30,4 +30,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gramps_online.settings')
 
+# pylint: disable=invalid-name
 application = get_wsgi_application()
